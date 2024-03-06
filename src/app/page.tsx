71b01b3ef00c43e82583496fe95257e97d5418fd
Noel Mejia <noel3098@gmail.com>
2024-03-05 23:11:41 -0400

    Ejemplo de Boton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      Space-Ship-Forum
+    <main className="flex justify-center items-center h-screen">
+      <Button variant={"outline"}>Space-Ship-Forum</Button>
     </main>
   );
 }
